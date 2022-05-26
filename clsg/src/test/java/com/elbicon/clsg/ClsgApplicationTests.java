3997241c518b33ed72fbd3e2198886fe17205f6a
Leslie Fleming <leslie_fleming@elbicon.com>
2022-05-26 16:40:33 +0100

    clsg app

---
+++ clsg/src/test/java/com/elbicon/clsg/ClsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbicon.clsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
